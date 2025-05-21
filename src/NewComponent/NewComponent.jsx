29090f6361cd65d09f3dc509d0136c01c296999c
Mohan Dhanasekaran <mohandhanasekaran71@gmail.com>
2025-05-21 10:23:18 +0530

    message

--- src/NewComponent/NewComponent.jsx
+++ src/NewComponent/NewComponent.jsx
@@ -2,25 +2,26 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import "./NewComponent.css";
 import logo from "../Assets/logo1.png";
-import logo1 from "../Assets/Hardik.jpg";
-import logo2 from "../Assets/side-logo-1.png";
+// import logo1 from "../Assets/Hardik.jpg";
+// import logo2 from "../Assets/side-logo-1.png";
 import logo3 from "../Assets/InSightsSuccess.png";
-import logo4 from "../Assets/Enterprise World.png";
+import logo4 from "../Assets/Enterprise-World.png";
 import logo5 from "../Assets/The New Indian Express.png";
 import logo6 from "../Assets/Akkineni foundation of America.jpeg";
-import logo7 from "../Assets/IRCLASS.jpeg";
-import logo8 from "../Assets/NASSCOM.png";
-import logo9 from "../Assets/BQC.png";
+// import logo7 from "../Assets/IRCLASS.jpeg";
+// import logo8 from "../Assets/NASSCOM.png";
+// import logo9 from "../Assets/BQC.png";
 import logo10 from "../Assets/Indian Business Congress.png";
 import logo11 from "../Assets/Entrepreneurs Council of India.png";
-
 import {
-  FaFacebook,
-  FaLinkedin,
+  FaFacebookF,
+  FaLinkedinIn,
   FaTwitter,
   FaGlobe,
   FaPhone,
   FaMapMarkerAlt,
+  FaGoogle,
+  FaEnvelope ,
 } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 import { useRef } from "react";
@@ -28,7 +29,7 @@ import html2canvas from "html2canvas";
 
 const NewComponent = () => {
   const location = useLocation();
-  const { username, mobile, mail, image, workType } = location.state || {};
+  const { username, image } = location.state || {};
   const captureRef = useRef(null); // Reference for capturing content
 
   const handleDownloadWithHtml2Canvas = () => {
@@ -63,210 +64,92 @@ const NewComponent = () => {
 
   return (
     <div className="Apps" ref={captureRef}>
-      <div className="left-panel">
-        <div className="logo">
-          <img src={logo} alt="" />
-        </div>
-
-        <div className="img-s">
+      <div className="top-container">
+        <div className="left-side">
           {image && (
-            <div className="image-heads">
+            <div className="image-head">
               <img src={image} alt="Uploaded" className="profile-pic" />
             </div>
           )}
         </div>
-
-        <div className="icon-s">
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaFacebook size={35} color="black" />
-          </a>
-          <a
-            href="https://linkedin.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaLinkedin size={35} color="#0A66C2" />
-          </a>
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaTwitter size={35} color="#1DA1F2" />
-          </a>
-          <a
-            href="https://www.example.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FaGlobe size={35} color="black" />
-          </a>
-        </div>
-      </div>
-
-      <div className="middle-panel">
-        <div className="username-s">
-          <h1>{username || "USERNAME"}</h1>
-          <h3>{workType || "HR & ADMIN"}</h3>
-        </div>
-
-        <div className="contact-info">
-          <p>
-            <FaPhone className="icon-1" /> +91 {mobile}
-          </p>
-          <p>
-            <FiMail className="icon-1" /> {mail}
-          </p>
-          <p>
-            <FaGlobe className="icon-1" /> www.necurity.com
-          </p>
-          <p>
-            <FaMapMarkerAlt className="icon-1" /> 6/13, North Avenue,Kesavaperumalpuram,
-            Raja Annamalai Puram, Chennai-28.
-          </p>
-        </div>
-      </div>
-
-      <div className="right-panel">
-        <div className="awards-header">
-          <h2>Awards and Achievements</h2>
-        </div>
-        <div className="awards-middle-header">
-          
-          <div className="split-awards-middle para-4">
-            <div className="left-s">
-              <div className="awards-middle">
-                <img src={logo2} alt="" />
-              </div>
-              <div className="awards-content">
-                <p>
-                  Listed as " Top 10 Trusted Cyber Security Companies in India"
-                  by Insight Success Magazine 2019
-                </p>
-              </div>
-              <div className="company-logo">
-                <img src={logo3} alt="" />
-              </div>
+        <div className="right-side">
+          <div className="head-details">
+            <div className="header-names">
+              <h1 className="username-s">Ishwar Prasad Bhat</h1>
+              <h4 className="sub-head">CEO & Founder</h4>
+              <p className="para-s">
+                Necurity Solutions Network Security Private Limited
+              </p>
             </div>
-
-            <div className="left-s1">
-              <div className="awards-middle1">
-                <img src={logo2} alt="" />
-              </div>
-              <div className="awards-content1">
-                <p className="para-1">Covered by the Enterprise World</p>
-              </div>
-              <div className="company-logo1">
-                <img src={logo4} alt="" />
-              </div>
-            </div>
-
-            <div className="left-s2">
-              <div className="awards-middle2">
-                <img src={logo2} alt="" />
-              </div>
-              <div className="awards-content2">
-                <p>
-                  Covered by the New Indian Express Newspaper for Extensive work
-                  on Cyber Security
-                </p>
-              </div>
-              <div className="company-logo3">
-                <img src={logo5} alt="" />
-              </div>
+            <div className="image-s">
+              <img src={logo} alt="NO Logo" />
             </div>
           </div>
-
-          <div className="split-awards-middle para-3">
-            <div className="left-s">
-              <div className="awards-middle">
-                <img src={logo2} alt="" />
-              </div>
-              <div className="awards-content">
-                <p className="para-1">Best Cyber Security Expert by</p>
-              </div>
-              <div className="company-logo">
-                <img src={logo10} alt="" />
-              </div>
+          <div className="contact-details">
+            <div className="contact-inform">
+              <p className="ic-2">
+                <FaPhone
+                  style={{ transform: "scaleX(-1)" }}
+                  className="icon-1 ic-1"
+                />{" "}
+                +91 8122331401
+              </p>
+              <p>
+                <FaEnvelope  className="icon-1" /> ishwar@necurity.com{" "}
+              </p>
+              <p>
+                <FaGlobe className="icon-1" /> www.necurity.com
+              </p>
+              <p>
+                <FaMapMarkerAlt className="icon-1" /> Block 3, II Floor, Equinox Managed Workspaces, 6/13, North Ave, Kesavaperumalpuram, <br /> Raja Annamalai Puram, Chennai, Tamil Nadu 600028
+              </p>
             </div>
-
-            <div className="left-s1">
-              <div className="awards-middle1">
-                <img src={logo2} alt="" />
-              </div>
-              <div className="awards-content1">
-                <p className="para-1">Entrepreneurs of the year 2016</p>
-              </div>
-              <div className="company-logo4">
-                <img src={logo11} alt="" />
-              </div>
-            </div>
-
-            <div className="left-s2">
-              <div className="awards-middle2">
-                <img src={logo2} alt="" />
-              </div>
-              <div className="awards-content2">
-                <p className="para-2">
-                  Vinutna Ratna Award by Akkineni Foundation of USA
-                </p>
-              </div>
-              <div className="company-logo5">
-                <img src={logo6} alt="" />
-              </div>
-            </div>
-          </div>
-
-          <div className="split-awards-middle">
-            <div className="left-s">
-              <div className="awards-middle">
-                <img src={logo2} alt="" />
-              </div>
-              <div className="awards-content">
-                <p className="para-1">We are certified by IRCLASS</p>
-              </div>
-              <div className="company-logo6">
-                <img src={logo7} alt="" />
-              </div>
-            </div>
-
-            <div className="left-s1">
-              <div className="awards-middle1">
-                <img src={logo2} alt="" />
-              </div>
-              <div className="awards-content1">
-                <p className="para-1 fs-1">NASSCOM</p>
-              </div>
-              <div className="company-logo7">
-                <img src={logo8} alt="" />
-              </div>
-            </div>
-
-            <div className="left-s2">
-              <div className="awards-middle2">
-                <img src={logo2} alt="" />
-              </div>
-              <div className="awards-content2">
-                <p className="para-1">
-                  We are ISO 27001 certified Organisation
-                </p>
-              </div>
-              <div className="company-logo8">
-                <img src={logo9} alt="" />
-              </div>
+            <div className="company-inform">
+              <FaFacebookF className="icon-2" />
+              <FaTwitter className="icon-2" />
+              <FaLinkedinIn className="icon-2" />
+              <FaGoogle className="icon-2" />
             </div>
           </div>
         </div>
       </div>
-
-      {/* Download Button - excluded from screenshot */}
-      <button onClick={handleDownloadWithHtml2Canvas} className="download-btn">
-        Download
-      </button>
+      <div className="bottom-container">
+        <div className="awards-title">
+          <h1>Awards and Achievements</h1>
+        </div>
+        <div className="awards-card">
+          <div className="awards">
+            <p>Top 10 most trusted cyber threat solution providers by</p>
+            <img className="img-1" src={logo3} alt="NO image" />
+          </div>
+          <div className="awards">
+            <p>Top 10 disruptive companies to watch in 2019 by</p>
+            <img className="img-2" src={logo4} alt="NO image" />
+          </div>
+          <div className="awards">
+            <p>Exclusive cyber Security article featured by</p>
+            <img className="img-3" src={logo5} alt="NO image" />
+          </div>
+          <div className="awards">
+            <p>
+              Best cyber security expert <br /> by
+            </p>
+            <img className="img-4" src={logo10} alt="NO image" />
+          </div>
+          <div className="awards">
+            <p>
+              Entrepreneur of the year - 2016 <br /> by
+            </p>
+            <img className="img-5" src={logo11} alt="NO image" />
+          </div>
+          <div className="awards">
+            <p>
+              Vinutna Ratna <br /> Award  <br /> by
+            </p>
+            <img className="img-6" src={logo6} alt="NO image" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
